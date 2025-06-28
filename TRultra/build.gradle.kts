a1ab37eede4ae458e63ae03d0269e382103d3a5e
PyrusDrago <cengiz.games.mail@gmail.com>
2025-06-28 03:37:33 +0300

    Build 9104c8b92692f9c300cf56c20ccf68f1e94e60f6

---
+++ TRultra/build.gradle.kts
@@ -0,0 +1,16 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "Tüm tr depoları tek yerde"
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1
+    language = "tr"
+}
